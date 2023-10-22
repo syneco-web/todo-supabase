f47075df1a1c9faf877ce363e1859e25e4f87226
syneco-web <147792248+syneco-web@users.noreply.github.com>
2023-10-23 08:16:00 +0900

    login register

---
+++ pages/dashboard.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next'
+import { LogoutIcon } from '@heroicons/react/solid'
+import { supabase } from '../utils/supabase'
+import Layout from '../components/Layout'
+
+const Dashboard: NextPage = () => {
+    const signOut = () => {
+        supabase.auth.signOut()
+    }
+  return (
+    <Layout title="Dashboard">
+        <LogoutIcon onClick={signOut} className="mb-6 h-6 w-6 cursor-pointer text-blue-500" />
+        
+    </Layout>
+  )
+}
+
+export default Dashboard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,74 @@
 import type { NextPage } from 'next'
+import { useState, FormEvent } from 'react'
+import { BadgeCheckIcon, ShieldCheckIcon } from '@heroicons/react/solid'
+import { useMutateAuth } from '../hooks/useMutateAuth'
 import Layout from '../components/Layout'
-import Link from 'next/link'
 
-const Home: NextPage = () => {
+const Auth: NextPage = () => {
+  const [isLogin, setIsLogin] = useState(true)
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loginMutation,
+    registerMutation,
+  } = useMutateAuth()
+  
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (isLogin) {
+      loginMutation.mutate()
+    } else {
+      registerMutation.mutate()
+    }
+  }
   return (
-    <Layout title="Home">
-      {'Hello KoJo'}
+    <Layout title="Auth">
+      <ShieldCheckIcon className="w-12 h-12 mb-6 text-blue-500" />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input 
+            type="text"
+            required
+            className='my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="password"
+            required
+            className='my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="my-6 flex items-center justify-center text-sm">
+          <span
+            onClick={() => setIsLogin(!isLogin)}
+            className="cursor-pointerfont-medium hover:text-indigo-500"
+          >
+            change mode ?
+          </span>
+        </div>
+        <button
+          type="submit"
+          className='group relative flex w-full justify-center rounded-md bg-indigo-600 py-4 px-4 text-sm font-medium text-white hover:bg-indigo-700'
+        >
+          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <BadgeCheckIcon className="h-5 w-5" />
+            {isLogin ? 'Login' : 'Register'}
+          </span>
+
+        </button>
+      </form>
       
     </Layout>
   )
 }
 
-export default Home
+export default Auth
